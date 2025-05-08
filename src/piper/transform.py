b29a821562d2108d34b8a3f7f5c3867f85a8802d
Yelena Cox <yelenacox@gmail.com>
2025-05-08 15:07:06 -0500

    Error handling for version

--- src/piper/transform.py
+++ src/piper/transform.py
@@ -34,7 +34,7 @@ def buildfhir(config, dataset,
         "-mapping_file_spec", whistle_src, 
         "-lib_dir_spec", str(projection_directory),
         "-verbose",
-        "-output_dir", outdir
+        "-output_dir", str(outdir)
     ]
     result = subprocess.run(command)
     if result.returncode != 0:
@@ -70,7 +70,7 @@ def run(args=None):
     parser.add_argument(
         "-i", 
         "--dataset-input", 
-        default=None,
+        required=True,
         type=FileType("rt"),
         help="Dataset in JSON format organized according the projection "\
           "expectations."
@@ -93,7 +93,7 @@ def run(args=None):
     # TODO: Add all of the necessary arguments
     parser.add_argument(
         "--output-dir",
-        default="output/whistle-output/study",
+        default=None,
         type=str
     )
 
@@ -135,49 +135,60 @@ def run(args=None):
     args = parser.parse_args(args) 
     # pdb.set_trace()
     
+    try:
+    
+      # Build the config 
+      config = load_piper_config(args.config)
 
-    # Build the config 
-    config = load_piper_config(args.config)
-
-    # If DATASET_INPUT is None, pull harmony out of config
-    if (args.dataset_input is None):
-        args.dataset_input = config['harmony']['filename']
-    else:
-        args.dataset_input = args.dataset_input.name
+      # If DATASET_INPUT is None, pull harmony out of config
+      if (args.dataset_input is None):
+          args.dataset_input = config['harmony']['filename']
+      else:
+          args.dataset_input = args.dataset_input.name
 
 
-    if (args.harmony is None):
-        args.harmony = config['harmony']['path']
+      if (args.harmony is None):
+          args.harmony = config['harmony']['path']
 
-    if (args.projection_version is None):
-        args.projection_version = config['projections']['default_version']
 
-# Check if a module is specified by the user, otherwise use the default
-    if args.module is None:
-        args.module = config['projections']['default_module']
-    elif args.module not in config['projections']['modules']:
-        raise KeyError(f"Module '{args.module}' not found in config file.")
-    elif not Path(config['projections']['modules'][args.module]['path']).exists():
-        # User input module found in config file, but the path to that file cannot be found (i.e. file does not exist)
-        raise FileNotFoundError(f"Module '{args.module}' does not exist.")
+      proj_modules = config['projections']['modules']   
+  # Check if a module is specified by the user, otherwise use the default
+      if args.module is None:
+          args.module = config['projections']['default_module']
+      elif args.module not in config['projections']['modules']:
+          raise KeyError(f"Module '[green]{args.module}[/green]' not found in config file. Available modules: [cyan]{', '.join(proj_modules.keys())}[/cyan]")
+      elif not Path(proj_modules[args.module]['path']).exists():
+          # User input module found in config file, but the path to that file cannot be found (i.e. file does not exist)
+          raise FileNotFoundError(f"Module '[green]{args.module}[/green]' not found.")
+      
+      if (args.projection_version is None):
+          args.projection_version = config['projections']['default_version']
+      elif not (Path(config['projections']['modules'][args.module]['path']) / args.projection_version).exists():
+          raise FileNotFoundError(f"Version '[green]{args.projection_version}[/green]' not found.")
 
-    proj_module = config['projections']['modules'][args.module]    
+      proj_module = config['projections']['modules'][args.module]
 
-    # use Path object to make output directory that's the same as the output directory.
-    # if it doesn't exist, create it. 
-    if (args.output_dir is None):
-        output_dir = Path(proj_module['output_dir'])
-    else:    
-        output_dir = Path(args.output_dir)
-    output_dir.mkdir(parents=True, exist_ok=True)
+      # use Path object to make output directory that's the same as the output directory.
+      # if it doesn't exist, create it. 
+      if (args.output_dir is None):
+          output_dir = Path(proj_module['output_dir'])
+      else:    
+          output_dir = Path(args.output_dir)
+      output_dir.mkdir(parents=True, exist_ok=True)
 
-    # Call play with the appropriate parameters
-    buildfhir(
-        config=config, 
-        dataset=args.dataset_input, 
-        harmony_dir=args.harmony,
-        module_path=proj_module['path'],
-        whistle_src=proj_module['whistle_src'],
-        outdir=output_dir,
-        projection_version=args.projection_version
-    )
+
+      # Call play with the appropriate parameters
+      buildfhir(
+          config=config, 
+          dataset=args.dataset_input, 
+          harmony_dir=args.harmony,
+          module_path=proj_module['path'],
+          whistle_src=proj_module['whistle_src'],
+          outdir=output_dir,
+          projection_version=args.projection_version
+      )
+    except Exception as e:
+      msg=str(e)
+      if msg[0] == '"':
+          msg=str(e)[1:-1]
+      print(f"[white]{msg}[/white]")
